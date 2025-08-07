1eff594f3bcd04023cc656b32b69fb57f5d3239a
betulyıldırım <betulyldrmm32@example.com>
2025-08-07 18:16:28 +0300

    güncelleme

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,5 @@
 // src/config/api.js
-const API_URL = process.env.REACT_APP_API_URL || "https://backendd12345-psi.vercel.app";
+export const API_URL = 'https://shop-mind-6mf5-dyt5ppllk-betuls-projects-5b7c9a73.vercel.app';
+
 
 export { API_URL };
